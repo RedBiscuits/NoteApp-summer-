09493ba7a035e363602cbf4e426cd808746023ca
RedBiscuits <84057046+RedBiscuits@users.noreply.github.com>
2022-08-20 10:52:40 +0200

    SQLite + Home

--- app/src/main/java/com/datastructures/notesapp/MainActivity.java
+++ app/src/main/java/com/datastructures/notesapp/MainActivity.java
@@ -1,14 +1,275 @@
 package com.datastructures.notesapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import com.datastructures.notesapp.adapters.NotesAdapter;
+import com.datastructures.notesapp.database.DatabaseHelper;
+import com.datastructures.notesapp.pojo.Note;
+import com.datastructures.notesapp.utils.MyDividerItemDecoration;
+import com.datastructures.notesapp.utils.RecyclerTouchListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NotesAdapter mAdapter;
+    private List<Note> notesList = new ArrayList<>();
+    private CoordinatorLayout coordinatorLayout;
+    private RecyclerView recyclerView;
+    private TextView noNotesView;
+    ItemTouchHelper.SimpleCallback simpleItemTouchCallback;
+    ItemTouchHelper itemTouchHelper;
+
+    private DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        coordinatorLayout = findViewById(R.id.coordinator_layout);
+        recyclerView = findViewById(R.id.recycler_view);
+        noNotesView = findViewById(R.id.empty_notes_view);
+
+        db = new DatabaseHelper(this);
+
+        notesList.addAll(db.getAllNotes());
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(view -> showNoteDialog(false, null, -1));
+
+        mAdapter = new NotesAdapter(this, notesList);
+        RecyclerView.LayoutManager mLayoutManager = new StaggeredGridLayoutManager(
+                2 , StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+//        recyclerView.addItemDecoration(new MyDividerItemDecoration(this, LinearLayoutManager.VERTICAL, 16));
+        recyclerView.setAdapter(mAdapter);
+
+        toggleEmptyNotes();
+
+        setupItemTouchHelper();
+
+    }
+
+    /*
+    * Setting up swipes
+    * Left -> Edit note
+    * Right -> Delete note
+    * */
+    private void setupItemTouchHelper() {
+        simpleItemTouchCallback= new ItemTouchHelper.SimpleCallback(
+                0, ItemTouchHelper.LEFT
+                | ItemTouchHelper.RIGHT) {
+
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView,
+                                  @NonNull RecyclerView.ViewHolder viewHolder,
+                                  @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                int position = viewHolder.getAdapterPosition();
+                if(swipeDir == ItemTouchHelper.LEFT) {
+                    deleteNote(position);
+                }
+                else{
+                    showNoteDialog(true, notesList.get(position), position);
+                }
+            }
+
+            @Override
+            public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                                    float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+                Drawable icon;
+                ColorDrawable background;
+                View itemView = viewHolder.itemView;
+                int backgroundCornerOffset =21;
+
+                if(dX > 0){
+                    icon = ContextCompat.getDrawable(getBaseContext(),R.drawable.edit);
+                    background= new ColorDrawable(ContextCompat.getColor(getBaseContext(),R.color.colorPrimary));
+                }else {
+                    icon = ContextCompat.getDrawable(getBaseContext(),R.drawable.delete);
+                    background= new ColorDrawable(Color.RED);
+                }
+
+                int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight())/2;
+                int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight())/2;
+                int iconBottom = iconTop + icon.getIntrinsicHeight();
+
+                if(dX>0){//right
+                    int iconLeft = itemView.getLeft() + iconMargin;
+                    int iconRight = itemView.getLeft() + iconMargin + icon.getIntrinsicWidth();
+                    icon.setBounds(iconLeft,iconTop,iconRight,iconBottom);
+                    background.setBounds(itemView.getLeft(),itemView.getTop()
+                            , itemView.getLeft() + ((int)dX) + backgroundCornerOffset,itemView.getBottom());
+                }else if(dX<0){//left
+                    int iconLeft = itemView.getRight() - iconMargin - icon.getIntrinsicWidth();
+                    int iconRight = itemView.getRight() - iconMargin;
+                    icon.setBounds(iconLeft,iconTop,iconRight,iconBottom);
+                    background.setBounds(itemView.getRight() + ((int)dX) - backgroundCornerOffset,itemView.getTop()
+                            , itemView.getRight() ,itemView.getBottom());
+
+                }else{
+                    background.setBounds(0,0,0,0);
+                }
+                background.draw(c);
+                icon.draw(c);
+            }
+        };
+
+        itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+    }
+
+    /**
+     * Inserting new note in db
+     * and refreshing the list
+     */
+    private void createNote(String note) {
+        // inserting note in db and getting
+        // newly inserted note id
+        long id = db.insertNote(note);
+
+        // get the newly inserted note from db
+        Note n = db.getNote(id);
+
+        if (n != null) {
+            // adding new note to array list at 0 position
+            notesList.add(0, n);
+
+            // refreshing the list
+            mAdapter.notifyDataSetChanged();
+
+            toggleEmptyNotes();
+        }
+    }
+
+    /**
+     * Updating note in db and updating
+     * item in the list by its position
+     */
+    private void updateNote(String note, int position) {
+        Note n = notesList.get(position);
+        // updating note text
+        n.setNote(note);
+
+        // updating note in db
+        db.updateNote(n);
+
+        // refreshing the list
+        notesList.set(position, n);
+        mAdapter.notifyItemChanged(position);
+
+        toggleEmptyNotes();
+    }
+
+    /**
+     * Deleting note from SQLite and removing the
+     * item from the list by its position
+     */
+    private void deleteNote(int position) {
+        // deleting the note from db
+        db.deleteNote(notesList.get(position));
+
+        // removing the note from the list
+        notesList.remove(position);
+        mAdapter.notifyItemRemoved(position);
+
+        toggleEmptyNotes();
+    }
+
+
+    /**
+     * Shows alert dialog with EditText options to enter / edit
+     * a note.
+     * when shouldUpdate=true, it automatically displays old note and changes the
+     * button text to UPDATE
+     */
+    private void showNoteDialog(final boolean shouldUpdate, final Note note, final int position) {
+        LayoutInflater layoutInflaterAndroid = LayoutInflater.from(getApplicationContext());
+        View view = layoutInflaterAndroid.inflate(R.layout.note_dialog, null);
+
+        AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilderUserInput.setView(view);
+
+        final EditText inputNote = view.findViewById(R.id.note);
+        TextView dialogTitle = view.findViewById(R.id.dialog_title);
+        dialogTitle.setText(!shouldUpdate ? getString(R.string.lbl_new_note_title) : getString(R.string.lbl_edit_note_title));
+
+        if (shouldUpdate && note != null) {
+            inputNote.setText(note.getNote());
+        }
+        alertDialogBuilderUserInput
+                .setCancelable(false)
+                .setPositiveButton(shouldUpdate ? "update" : "save", (dialogBox, id) -> {
+
+                })
+                .setNegativeButton("cancel",
+                        (dialogBox, id) -> dialogBox.cancel());
+
+        final AlertDialog alertDialog = alertDialogBuilderUserInput.create();
+        alertDialog.show();
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(v -> {
+            // Show toast message when no text is entered
+            if (TextUtils.isEmpty(inputNote.getText().toString())) {
+                Toast.makeText(MainActivity.this, "Enter note!", Toast.LENGTH_SHORT).show();
+                return;
+            } else {
+                alertDialog.dismiss();
+            }
+
+            // check if user updating note
+            if (shouldUpdate && note != null) {
+                // update note by it's id
+                updateNote(inputNote.getText().toString(), position);
+            } else {
+                // create new note
+                createNote(inputNote.getText().toString());
+            }
+        });
+    }
+
+    /**
+     * Toggling list and empty notes view
+     */
+    private void toggleEmptyNotes() {
+        // you can check notesList.size() > 0
+
+        if (db.getNotesCount() > 0) {
+            noNotesView.setVisibility(View.GONE);
+        } else {
+            noNotesView.setVisibility(View.VISIBLE);
+        }
     }
 }
